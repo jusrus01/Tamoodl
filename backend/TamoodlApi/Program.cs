d8dcbe5f038402dfe07bb3f84ebe802dee108069
jusrus01 <jjustinas.ruslys@gmail.com>
2021-09-10 20:22:27 +0300

    Seeded data

--- backend/TamoodlApi/Program.cs
+++ backend/TamoodlApi/Program.cs
@@ -3,17 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using TamoodlApi.Seeds;
 
 namespace TamoodlApi
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public async static Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using(var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var logger = services.GetRequiredService<ILoggerFactory>()
+                    .CreateLogger("app");
+
+                try
+                {
+                    var userManager = services.GetRequiredService<UserManager<IdentityUser>>();
+                    var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+
+                    await DefaultRoles.SeedRolesAsync(userManager, roleManager);
+                    await DefaultRoles.SeedStudentAsync(userManager, roleManager);
+                    await DefaultRoles.SeedTeacherAsync(userManager, roleManager);
+                    await DefaultRoles.SeedAdminAsync(userManager, roleManager);
+
+                    logger.LogInformation("Success");
+
+                }
+                catch
+                {
+                    logger.LogWarning("An error occured seeding the database.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
